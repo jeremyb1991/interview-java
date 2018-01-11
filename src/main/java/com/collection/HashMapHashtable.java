7e68749037bc9048868b2f3d51467ad87ef71642
b08092007 <jeremyb.git.1991@gmail.com>
2018-01-11 21:13:11 +0800

    commit collection and thread

---
+++ src/main/java/com/collection/HashMapHashtable.java
@@ -0,0 +1,24 @@
+package collection;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+/**
+ * Created by xiaohe on 1/11/18.
+ */
+public class HashMapHashtable {
+
+    public static void main(String args[]){
+        Hashtable<Integer,String> hm=new Hashtable<Integer,String>();
+
+        hm.put(100,"Amit");
+        hm.put(102,"Ravi");
+        hm.put(101,"Vijay");
+        hm.put(103,"Rahul");
+
+        for(Map.Entry m:hm.entrySet()){
+            System.out.println(m.getKey()+" "+m.getValue());
+        }
+    }
+
+}
