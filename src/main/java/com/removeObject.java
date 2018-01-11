b859a58af2a5a409096087ba4d080f7f4be4d7b3
b08092007 <jeremyb.git.1991@gmail.com>
2018-01-11 18:01:56 +0800

    How to remove an element from ArrayList in Java

---
+++ src/main/java/com/removeObject.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+/**
+ * Created by xiaohe on 1/11/18.
+ */
+public class removeObject {
+
+    public static void main(String[] args) {
+
+        // create an empty array list with an initial capacity
+        ArrayList<String> arrlist = new ArrayList<String>(5);
+
+        // use add() method to add values in the list
+        arrlist.add("G");
+        arrlist.add("E");
+        arrlist.add("F");
+        arrlist.add("M");
+        arrlist.add("E");
+
+        System.out.println("Size of list: " + arrlist.size());
+
+        // let us print all the values available in list
+        for (String value : arrlist) {
+            System.out.println("Value = " + value);
+        }
+
+        // Removes first occurrence of "E"
+        arrlist.remove("E");
+
+
+
+        System.out.println("Now, Size of list: " + arrlist.size());
+
+        // let us print all the values available in list
+        for (String value : arrlist) {
+            System.out.println("Value = " + value);
+        }
+    }
+
+}
